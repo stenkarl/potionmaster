4144d8d85ec5843af581ae167d96cbeef73d9f42
stenkarl <sten_karl@yahoo.com>
2020-05-25 11:36:46 -0500

    Added color prefabs

---
+++ Assets/Scripts/FourCircles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourCircles : MonoBehaviour
+{
+    public GameObject red;
+    public GameObject blue;
+
+    private List<GameObject> colors = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Fill(string color) {
+        Debug.Log("color " + color);
+        GameObject current = Instantiate(getForName(color), this.transform, false);
+        float x = -2.9f + (1.95f * colors.Count);
+        current.transform.localPosition = new Vector3(x, 0.0f);
+        colors.Add(current);
+    }
+
+    private GameObject getForName(string color) {
+        if (color.Contains("Red")) {
+            return red;
+        }
+        return blue;
+    }
+}
---
+++ Assets/Scripts/Orange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orange : MonoBehaviour {
+
+    private FourCircles fourCircles;
+
+    // Start is called before the first frame update
+    void Start() {
+        fourCircles = GameObject.Find("FourCircles").GetComponent<FourCircles>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMouseDown() {
+        Debug.Log("clicked " + this.name);
+        fourCircles.Fill(this.name);
+     
+    }
+}
